5b1b0afc7f07ab3656a0cbc8f7be70ef00ac2e1f
Oscar Barrera <oscbco@gmail.com>
2020-04-28 12:39:07 -0600

    MessageTable makes a POST request to change the status of messages when its callback changeMessages is called Changed expired token

--- source/components/MessagesTable/MessagesTable.js
+++ source/components/MessagesTable/MessagesTable.js
@@ -1,24 +1,75 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+
 import DenseTable from '../DenseTable/DenseTable';
+import MessageActions from '../MesssageActions/MessageActions';
 // import fetch from 'node-fetch';
 
 export default function MessagesTable (props) {
+  const [selected, setSelected] = useState(new Set([]));
+  const [fetching, setFetching] = useState(false);
   useEffect(() => {
+    fetchMessages();
+  }, [props.vmBoxId]);
+
+  const fetchMessages = () => {
     const requestUrl = `${props.serverUrl}/accounts/${props.accountId}/vmboxes/${props.vmBoxId}/messages?paginate=true`;
+    setFetching(true);
     fetch(requestUrl, { headers: props.headers })
       .then(response => response.json())
-      .then(({ data }) => props.setMessages(data))
-      .catch(console.log)
-  }, [props.vmBoxId]);
+      .then(({ data }) => {
+        if (Array.isArray(data)) {
+          props.setMessages(data);
+          setFetching(false)
+        }
+        return null;
+      })
+      .catch((error) => {
+        console.log(error)
+        setFetching(false);
+      });
+  }
+
+  const changeMessageStatus = (status) => {
+    const requestUrl = `${props.serverUrl}/accounts/${props.accountId}/vmboxes/${props.vmBoxId}/messages`;
+    setFetching(true);
+    fetch(requestUrl, {
+      method: 'POST',
+      headers: props.headers,
+      body: JSON.stringify({
+        data: {
+          folder: status,
+          messages: [...selected]
+        }
+      })
+    }).then(response => response.json())
+      .then(() => fetchMessages())
+      .catch((error) => {
+        console.log(error)
+        setFetching(false);
+      });
+  }
+
+  const selectMessage = (event) => {
+    if (event.target.checked) {
+      setSelected(new Set(selected.add(event.target.name)));
+    } else {
+      selected.delete(event.target.name);
+      setSelected(new Set(selected));
+    }
+  }
+
+  console.log(selected);
 
   if (!props.messages) {
     return null;
   }
+
   return (
     <div>
       <h1>Voicemail Messages</h1>
-      <DenseTable rows={props.messages} />
+      <MessageActions changeMessageStatus={changeMessageStatus} fetching={fetching} selected={selected} />
+      <DenseTable rows={props.messages} selectMessage={selectMessage} fetching={fetching} selected={selected} />
     </div>
   );
 }
